a66448c5a47b5e00bd5f810af515cf40e4c41054
Aditya Burugu <65937016+AdityaBurugu@users.noreply.github.com>
2020-09-05 00:33:07 +0530

    Delete test7.py

--- Face&Motion_Detection/test7.py
+++
@@ -1,9 +0,0 @@
-import os
-import threading
-class Thread(threading.Thread):
-    os.system("python Motion_Detection_File.py")
-
-#os.system("python Motion_Detection_File.py")
-Thread.start()
-Thread.start()
-
